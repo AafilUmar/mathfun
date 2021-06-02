280a98861f459d471189b4fdf458761781af7379
AFIL K UMMER <afil.umar007@gmail.com>
2021-06-03 00:02:18 +0530

    First commit

---
+++ mathfun/src/main/java/com/example/mathfun/MathLib.java
@@ -0,0 +1,22 @@
+package com.example.mathfun;
+
+public class MathLib {
+
+
+    public int sumOfTwo(int a,int b){
+        return a+b;
+    }
+    public int sumOfThree(int a,int b,int c){
+        return a+b+c;
+    }
+    public int productOfTwo(int a,int b){
+        return a*b;
+    }
+    public int divOfTwo(int a,int b){
+        return a/b;
+    }
+    public int subOfTwo(int a,int b){
+        return a-b;
+    }
+
+}
